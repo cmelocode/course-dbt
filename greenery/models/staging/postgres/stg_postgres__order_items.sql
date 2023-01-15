cc74477d54cf8258aac7bdad4dc2c639a4b6c7a2
cmelocode <carlmellow@gmail.com>
2023-01-15 22:12:11 +0000

    add schema.yml file

--- greenery/models/staging/postgres/stg_postgres__order_items.sql
+++ greenery/models/staging/postgres/stg_postgres__order_items.sql
@@ -1,5 +1,6 @@
 with order_items as (select * from {{ source('postgres', 'order_items') }} )
-select order_id
+select {{ dbt_utils.generate_surrogate_key(['order_id' ,'product_id']) }} as  order_item_id
+     , order_id
      , product_id
      , quantity
 from order_items
